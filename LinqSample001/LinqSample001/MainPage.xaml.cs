fcd8e2f064b9a0b8e9009c3ce2fc56a4692c7852
謙太 可士村 <150365@jc-21.jp>
2017-12-07 10:35:07 +0900

    インサートの後にボタン系再配置

--- LinqSample001/LinqSample001/MainPage.xaml.cs
+++ LinqSample001/LinqSample001/MainPage.xaml.cs
@@ -80,10 +80,67 @@ namespace LinqSample001
             Content = layout;
         }
 
+        /************************************インサートボタンが押されたら（長い）*****************************************/
         public void InsertClicked(object sender, EventArgs e)
         {
             //Userテーブルに適当なデータを追加する
             UserModel008.insertUser(1,InsertEntry.Text);
+
+            var layout = new StackLayout { HorizontalOptions = LayoutOptions.Center, Margin = new Thickness { Top = 100 } };
+
+            //ボタン再配置（見かけ上消えないようにする）
+            //-----------------------------インサートするテキストボックス-------------------
+            InsertEntry = new Entry
+            {
+                WidthRequest = 60
+            };
+            layout.Children.Add(InsertEntry);
+
+
+            //-------------------------------インサートボタン-------------------------------
+            var Insert = new Button
+            {
+                WidthRequest = 60,
+                Text = "INSERT",
+                TextColor = Color.Blue,
+            };
+            layout.Children.Add(Insert);
+            Insert.Clicked += InsertClicked;
+
+            //--------------------------------デリートボタン------------------------------
+            var Delete = new Button
+            {
+                WidthRequest = 60,
+                Text = "DELETE",
+                TextColor = Color.Blue,
+            };
+            layout.Children.Add(Delete);
+            Delete.Clicked += DeleteClicked;
+
+            //--------------------------------セレクトボタン------------------------------
+            var Select = new Button
+            {
+                WidthRequest = 60,
+                Text = "SELECT",
+                TextColor = Color.Blue,
+            };
+            layout.Children.Add(Select);
+            Select.Clicked += SelectClicked;
+
+            //-----------------------------selectした結果がnullじゃなかったら（=表の中身が入っていたらselectメソッドを実行）----------------------
+            if (UserModel008.selectUser() != null)
+            {
+                var query = UserModel008.selectUser();
+
+                foreach (var user in query)
+                {
+                    //Userテーブルの名前列をLabelに書き出す
+                    layout.Children.Add(new Label { Text = user.Id.ToString() });
+                    layout.Children.Add(new Label { Text = user.Name });
+                }
+            }
+            Content = layout;
+
 
             //Userテーブルの行データを取得
             var query = UserModel008.selectUser();
@@ -97,7 +154,8 @@ namespace LinqSample001
             Content = layout;
 
         }
-
+        
+        /********************************デリートボタンが押されたら************************************************************************/
         public void DeleteClicked(object sender, EventArgs e)
         {
             UserModel008.deleteUser(1);
